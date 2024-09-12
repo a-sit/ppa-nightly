7a48f53e8b5da7f575759d1ab97922550ba46bb7
Stefan <stefan.more@a-sit.at>
2024-09-12 20:59:46 +0200

    PPA Publish Workflow: Cleanup scripts

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
+
 # config:
 SIGNINGKEYID="E1F8330A475CD21A"
 
@@ -19,18 +20,25 @@ SIGNINGKEYID="E1F8330A475CD21A"
 #fi
 #NEWDEB=`echo $NEWDEB | xargs`
 
-
+echo "Update Package index ..."
 dpkg-scanpackages --multiversion . > Packages
-
 gzip -k -f Packages
+
+echo "Create Release file ..."
 apt-ftparchive release . > Release
 
+echo "Sign Release file ..."
 gpg --default-key $SIGNINGKEYID -abs -o - Release > Release.gpg
+echo "Sign Release file with inline signature ..."
 gpg --default-key $SIGNINGKEYID --clearsign -o - Release > InRelease
 
+echo "Update PPA .list file ..."
 echo "deb https://a-sit/.github.io/ppa-nightly ./" > file.list
 
 
+echo "Commit changes back to PPA repo ..."
 git add Packages Packages.gz Release Release.gpg InRelease file.list
 #git add $NEWDEB
 git commit -m "Update PPA" #-m "add $NEWDEB"
+
+echo "update.sh done!"
